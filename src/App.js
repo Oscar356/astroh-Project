96ea938f0d23d841aee179cf3912b9bbabdae906
Oscar <68169051+Oscar356@users.noreply.github.com>
2021-02-11 17:02:11 -0800

    Navigation Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/home";
+import ProfilePage from "./pages/profile";
+import EditProfilePage from "./pages/edit-profile";
+import LoginPage from "./pages/login";
+import SignUpPage from "./pages/signup";
+import NotFoundPage from "./pages/not-found";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/:username" component={ProfilePage} />
+        <Route path="/accounts/edit" component={EditProfilePage} />
+        <Route path="/accounts/login" component={LoginPage} />
+        <Route path="/accounts/emailsignup" component={SignUpPage} />
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/not-found.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/shared/Navbar";
+
+function NotFoundPage() {
+  return (
+    <>
+      <Navbar />
+      <div>Not Found Page</div>
+    </>
+  );
+}
+
+export default NotFoundPage;
