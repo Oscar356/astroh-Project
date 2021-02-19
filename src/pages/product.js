7357b598bb31b53efdc827ac3a0289d92f2f60b9
Oscar <68169051+Oscar356@users.noreply.github.com>
2021-02-18 21:10:31 -0800

    Register Updates

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,27 +1,50 @@
 import React from "react";
 import Layout from "../components/shared/Layout";
 import { useParams } from "react-router-dom";
+import "./css/product.css";
+import blueHoodie from "../images/BlueSweater.jpg";
 
 function ProductPage() {
   const { item } = useParams();
+  var dummyData = {
+    name: "Blue Sweater",
+    price: "39.99",
+    imageUrl: blueHoodie
+  };
 
-  function productCard(productType) {
+  function productCard(productInfo) {
     return (
-      <div class="card">
-        <img src="img_avatar.png" alt="Product" 
-        // style="width:100%"
-         />
-        <div class="container">
-          <h4>
-            <b>John Doe</b>
-          </h4>
-          <p>Architect Engineer</p>
+      <div className="card">
+        <img
+          className="productImage"
+          src={productInfo.imageUrl}
+          alt="Product"
+        />
+
+        <div className="cardText">
+          <h6>
+            <b>{productInfo.name}</b>
+          </h6>
+          <p>{`$${productInfo.price}`}</p>
         </div>
       </div>
     );
   }
 
-  return <Layout>{productCard(item)}</Layout>;
+  function capitalize(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+  }
+
+  return (
+    <Layout>
+      <div className="cardContainer">
+        <h6 className="pageTitle">
+          {capitalize(item)}
+        </h6>
+        {Array.from({ length: 4 }, () => productCard(dummyData))}
+      </div>
+    </Layout>
+  );
 }
 
 export default ProductPage;
